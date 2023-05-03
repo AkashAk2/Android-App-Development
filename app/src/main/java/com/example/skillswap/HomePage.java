1aa8ccf460d86bea1e60ba6aafc885a5c38dbb38
abal0041 <abal0041@student.monash.edu>
2023-05-03 17:50:14 +1000

    Initial commit Home Screen

---
+++ app/src/main/java/com/example/skillswap/HomePage.java
@@ -0,0 +1,48 @@
+package com.example.skillswap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        //Find buttons by their ids
+        Button loginButton = findViewById(R.id.loginButton);
+        Button signupButton = findViewById(R.id.signupButton);
+        Button guestButton = findViewById(R.id.guestButton);
+
+        //Setting click listeners for the buttons
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Login activity
+                Intent intent = new Intent(HomePage.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Signup activity
+                Intent intent = new Intent(HomePage.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        guestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
