7dbd447d31b010b12ba1fc164c9c8d90dd76f8b4
Ananya Alagh <ananyaalagh@MacBook-Pro-5.lan>
2023-05-12 11:21:20 +1000

    Adding date and ID attribute for skill graph data retrieval

--- app/src/main/java/com/example/skillswap/models/Skill.java
+++ app/src/main/java/com/example/skillswap/models/Skill.java
@@ -3,6 +3,9 @@ package com.example.skillswap.models;
 public class Skill {
     private String skill;
     private String category;
+    private boolean isAdded;
+    private long date;
+    private String skillId;
 
     public Skill() {
         // Default constructor required for Firebase
@@ -11,6 +14,30 @@ public class Skill {
         this.skill = skill;
         this.category = category;
     }
+
+    public boolean isAdded() {
+        return isAdded;
+    }
+
+    public void setAdded(boolean added) {
+        isAdded = added;
+    }
+
+    public Skill(String skill, String category, long date, boolean isAdded, String skillId) {
+        this.skill = skill;
+        this.category = category;
+        this.date = date;
+        this.isAdded = isAdded;
+        this.skillId = skillId;
+    }
+
+    public String getSkillId() {return skillId;}
+
+    public void setSkillId(String skillId) {this.skillId = skillId;}
+
+    public long getDate() {return date;}
+
+    public void setDate(long date) {this.date = date;}
 
     public String getSkill() {
         return skill;
@@ -33,6 +60,9 @@ public class Skill {
         return "Skill{" +
                 "skill='" + skill + '\'' +
                 ", category='" + category + '\'' +
+                ", isAdded=" + isAdded +
+                ", date=" + date +
+                ", skillId='" + skillId + '\'' +
                 '}';
     }
 }
